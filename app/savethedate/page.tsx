5a607dc3b8514368a402f4b1137218b6f8c84a12
jaeseokk <kang89kr@gmail.com>
2025-07-13 22:54:05 +0900

    Add savethedate page

---
+++ app/savethedate/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+
+const SaveTheDatePage = () => {
+  return (
+    <div className="min-h-dvh">
+      <main className="h-full items-center justify-center flex flex-col">
+        <Hero />
+      </main>
+    </div>
+  );
+};
+
+export default SaveTheDatePage;
